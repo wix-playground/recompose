91a7b7a5175e78fc53d1c3d5427b4dec719b2811
ykravv <yevheniikr@wix.com>
2020-06-18 17:41:36 +0300

    Replace componentWill lifecycle methods

--- src/packages/recompose/componentFromStream.js
+++ src/packages/recompose/componentFromStream.js
@@ -1,12 +1,46 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
+import { createSubscription } from 'create-subscription'
 import { createChangeEmitter } from 'change-emitter'
 import $$observable from 'symbol-observable'
 import { config as globalConfig } from './setObservableConfig'
 
+class SimpleBehaviorSubject {
+  currentValue = null
+
+  constructor(observable) {
+    this.observable = observable
+  }
+
+  getCurrentValue() {
+    return this.currentValue
+  }
+
+  subscribe(callback) {
+    const subscription = this.observable.subscribe({
+      next: value => {
+        this.currentValue = value
+        callback(value)
+      },
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }
+}
+
+const Subscription = createSubscription({
+  subscribe(simpleSubject, callback) {
+    return simpleSubject.subscribe(callback)
+  },
+
+  getCurrentValue(simpleSubject) {
+    return simpleSubject.getCurrentValue()
+  },
+})
+
 export const componentFromStreamWithConfig = config => propsToVdom =>
   class ComponentFromStream extends Component {
-    state = { vdom: null }
-
     propsEmitter = createChangeEmitter()
 
     // Stream of props
@@ -29,39 +63,27 @@ export const componentFromStreamWithConfig = config => propsToVdom =>
     // Stream of vdom
     vdom$ = config.toESObservable(propsToVdom(this.props$))
 
-    componentWillMount() {
-      // Subscribe to child prop changes so we know when to re-render
-      this.subscription = this.vdom$.subscribe({
-        next: vdom => {
-          this.setState({ vdom })
-        },
-      })
-      this.propsEmitter.emit(this.props)
+    constructor(props$) {
+      super(props)
+      this.subscription = new SimpleBehaviorSubject(this.vdom$)
+      this.propsEmitter.emit(props)
     }
 
-    componentWillReceiveProps(nextProps) {
-      // Receive new props from the owner
-      this.propsEmitter.emit(nextProps)
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-      return nextState.vdom !== this.state.vdom
-    }
-
-    componentWillUnmount() {
-      // Call without arguments to complete stream
-      this.propsEmitter.emit()
-
-      // Clean-up subscription before un-mounting
-      this.subscription.unsubscribe()
+    componentDidUpdate(prevProps) {
+      if (this.props !== prevProps) {
+        this.propsEmitter.emit(this.props)
+      }
     }
 
     render() {
-      return this.state.vdom
+      return (
+        <Subscription source={this.subscription}>
+          {vdom => vdom}
+        </Subscription>
+      )
     }
   }
 
-const componentFromStream = propsToVdom =>
-  componentFromStreamWithConfig(globalConfig)(propsToVdom)
+const componentFromStream = propsToVdom => componentFromStreamWithConfig(globalConfig)(propsToVdom)
 
 export default componentFromStream
